5867481f68d9f5ed6ab8c3402a94e3ad5138704a
denzildewet3-rgb <denzildewet3@gmail.com>
2025-10-09 11:41:27 +0200

    Add files via upload

---
+++ Guessing Game/guessingGame.js
@@ -0,0 +1,31 @@
+// var secret = 7
+
+// Make the number random
+var secret =  Math.floor(Math.random() * 20) + 1;
+
+let answer = prompt("Please guess the secret number (1-20)");
+var guess = parseInt(answer);
+
+// Please add an if statement which checks if the guess is correct, if it is, then display a message: Correct Guess!, otherwise “Sorry, incorrect Guess!
+
+// I have added the while loop here to run the alert until the correct number is given
+// while (guess !== secret) {
+//   if (guess === secret) {
+//   alert("Correct Guess!");
+// } else {
+//   alert("Sorry, incorrect Guess!");
+// }
+// guess = parseInt(prompt("Please guess the secret number (1-20)"));
+// }
+
+
+while (answer !== secret) {
+  if (answer < secret) {
+    alert("Incorrect, too low");
+  } else {
+    alert("Incorrect, too high");
+  }
+answer = parseInt(prompt("Please guess the secret number (1-20)"));
+}
+
+alert("Correct Guess!");
